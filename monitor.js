c5c5e5dbd77d88639945cf4e671e88044de26922
afwanizzulfadhoil <afwan2031@gmail.com>
2025-05-27 13:51:49 +0000

    domain

--- monitor.js
+++ monitor.js
@@ -5,7 +5,7 @@ const { version } = require('os');
 const app = express();
 const PORT = 3000;
 
-const publicIP = '36.78.116.84';
+const publicIP = 'mutusmp-regen.servegame.com';
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,9 +13,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// API Max Player
 app.get('/status', async (req, res) => {
     let response = {
+        server_name: "",
+        max_player: 0,
         java: {
             online: false,
             players: 0,
@@ -26,7 +27,6 @@ app.get('/status', async (req, res) => {
             players: 0,
             version: ""
         },
-        max_player: 0
     };
 
     try {
@@ -35,6 +35,7 @@ app.get('/status', async (req, res) => {
         response.java.players = resultJava.players.online;
         response.java.version = resultJava.version.name.split(" ")[1];
         response.max_player = resultJava.players.max;
+        response.server_name = resultJava.motd.clean;
     } catch (err) {
         console.error('Java status error:', err.message);
     }
@@ -50,7 +51,6 @@ app.get('/status', async (req, res) => {
     res.json(response);
 });
 
-//
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Status checker running at http://localhost:${PORT}`);
 });
