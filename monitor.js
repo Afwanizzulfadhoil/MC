6758d49c50bc11dc85a934ee7b6ae03f540b5ebe
afwanizzulfadhoil <afwan2031@gmail.com>
2025-05-27 12:47:54 +0000

    Update Max Player | API

--- monitor.js
+++ monitor.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { status, statusBedrock } = require('minecraft-server-util');
+const { version } = require('os');
 const app = express();
 const PORT = 3000;
 
@@ -12,37 +13,44 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/status/java', async (req, res) => {
-    try {
-        const result = await status(publicIP, 25565, { timeout: 500 });
-        res.json({ online: true, players: result.players.online });
-    } catch (error) {
-        res.json({ online: false });
-    }
-});
-
-app.get('/status/bedrock', async (req, res) => {
-    try {
-        const result = await statusBedrock(publicIP, 19132, { timeout: 500 });
-        res.json({ online: true, players: result.players.online });
-    } catch (error) {
-        res.json({ online: false });
-    }
-});
-
-
-// Testing
+// API Max Player
 app.get('/status', async (req, res) => {
-    let status = {};
+    let response = {
+        java: {
+            online: false,
+            players: 0,
+            version: ""
+        },
+        bedrock: {
+            online: false,
+            players: 0,
+            version: ""
+        },
+        max_player: 0
+    };
 
     try {
-        const result = await status(publicIP, 25565, { timeout: 500 });
-        res.json(result);
-    } catch (error) {
-        res.json({ online: false });
+        const resultJava = await status(publicIP, 25565, { timeout: 500 });
+        response.java.online = true;
+        response.java.players = resultJava.players.online;
+        response.java.version = resultJava.version.name.split(" ")[1];
+        response.max_player = resultJava.players.max;
+    } catch (err) {
+        console.error('Java status error:', err.message);
     }
-})
 
+    try {
+        const resultBedrock = await statusBedrock(publicIP, 19132, { timeout: 500 });
+        response.bedrock.online = true;
+        response.bedrock.version = resultBedrock.version.name;
+    } catch (err) {
+        console.error('Bedrock status error:', err.message);
+    }
+
+    res.json(response);
+});
+
+//
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Status checker running at http://localhost:${PORT}`);
 });
